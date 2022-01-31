ee44b2bab903a9e8aa6fad463f2a294826aee3c4
Rafael Nobre Queiroz <rafaelnobrequeiroz@gmail.com>
2022-01-31 15:20:09 -0300

    links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import {HomePage} from './pages/home'
 import {AboutPage} from './pages/about'
+import {Navbar} from './components/navbar'
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
