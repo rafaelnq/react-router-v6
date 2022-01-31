3e4c8d8169059f4dc30d093bc9d61baf33c5b27d
Rafael Nobre Queiroz <rafaelnobrequeiroz@gmail.com>
2022-01-31 15:14:55 -0300

    config routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {HomePage} from './pages/home'
+import {AboutPage} from './pages/about'
+
 function App() {
-  return <div className="App"></div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
